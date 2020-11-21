97f9928968d81f8e73ce2f57e7f48ca248010e2d
ch <mkmsacc@outlook.com>
2020-11-21 22:04:02 +0700

    Add DataAccessObjectParameterResolver

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,18 +1,48 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.IOException;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-@ExtendWith({ ExecutionContextExtension.class })
+@ExtendWith({ ExecutionContextExtension.class,
+		DatabaseOperationsExtension.class })
 public class PassengerTest {
 
+	private PassengerDao passengerDao;
+
+	public PassengerTest(PassengerDao passengerDao) {
+		this.passengerDao = passengerDao;
+	}
+
 	@Test
-	void testPassenger() throws IOException {
+	void testPassenger() {
 		Passenger passenger = new Passenger("123-456-789", "John Smith");
 		assertEquals("Passenger John Smith with identifier: 123-456-789",
 				passenger.toString());
+	}
+
+	@Test
+	void testInsertPassenger() {
+		Passenger passenger = new Passenger("123-456-789", "John Smith");
+		passengerDao.insert(passenger);
+		assertEquals("John Smith",
+				passengerDao.getById("123-456-789").getName());
+	}
+
+	@Test
+	void testUpdatePassenger() {
+		Passenger passenger = new Passenger("123-456-789", "John Smith");
+		passengerDao.insert(passenger);
+		passengerDao.update("123-456-789", "Michael Smith");
+		assertEquals("Michael Smith",
+				passengerDao.getById("123-456-789").getName());
+	}
+
+	@Test
+	void testDeletePassenger() {
+		Passenger passenger = new Passenger("123-456-789", "John Smith");
+		passengerDao.insert(passenger);
+		passengerDao.delete(passenger);
+		assertNull(passengerDao.getById("123-456-789"));
 	}
 
 }
